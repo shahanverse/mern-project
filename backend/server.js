72acacb85d05b6ff3c989c7964216f6f17419b3a
shahan <shahanverse@gmail.com>
2025-10-04 15:42:58 +0530

    npm install nodemon to fix refreshing backend without killing terminal

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.get("/api/notes", (req, res) => {
+  res.send("you got 5 notes")
+})
+
 app.listen(5001, () => {
   console.log("server started on PORT:5001");
 });
